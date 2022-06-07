299a7fd99320de932ca7303d65ed2dbbc8320154
BMarci <balogh.marton.21f@szlgbp.hu>
2022-06-07 15:09:43 +0200

    éohavananabhifhbreq

--- atlagszamito.js
+++ atlagszamito.js
@@ -1,31 +1,71 @@
+function CheckIfArray(obj)
+{
+    return obj.constructor.name == "Array";
+}
+
+function ArraysEqual(a, b)
+{
+    if(CheckIfArray(a) && CheckIfArray(b))
+    {
+        if(a.length === b.length)
+        {
+            for(let i = 0; i < a.length; i++)
+            {
+                if(!ArraysEqual(a[i], b[i])) return false;
+            }
+            return true;
+        }
+        else return false;
+    }
+    else return a === b;
+}
+
 function listRemoveSpecific(list, item, all = false)
 {
 	for(let i = 0; i < list.length; i++)
 	{
-		if(list[i] === item)
+		if(ArraysEqual(list[i], item))
 		{
 			list.splice(i, 1)
-			if(!all)
-			{
-				break;
-			}
+			if(!all) {
+				break; }
 		}
 	}
 }
-for(let i = 1; i < 6; i++) {document.querySelector(`#a${i}`).onclick = add(i); }
-function removeFromSum(jegy, suly)
+for(let i = 1; i < 6; i++) {document.querySelector(`#a${i}`).onclick = function() {add(i)}; }
+function removeFromSum(jegy, suly){listRemoveSpecific(sum, [jegy, suly], false); calcAv();}
+var sum = [];
+function calcAv()
 {
+	tsum = 0;
+	div = 0;
+	for(let i = 0; i < sum.length; i++)
+	{
+		tsum+=sum[i][0]*sum[i][1]/100;
+		div+=sum[i][1]/100;
 
+	}
+	document.querySelector("#eredmeny").innerText = tsum / div;
 }
-sum = [];
+var i = 0;
 function add(n) {
+	i++;
 	crn = document.createElement("li");
-	suly = document.querySelector("#jegysuly");
+	let suly = parseInt(document.querySelector("#jegysuly").value);
 	crn.innerText+=`${n}; ${suly} `;
 	crn_removebutton = document.createElement("button");
-	crn_removebutton.onclick=function() {crn.remove(); removeFromSum(jegy, suly) }
+	crn_removebutton.innerText = "-";
+	let j = i;
+	crn.id = `b${j}`;
+	crn_removebutton.onclick=function() {
+		document.querySelector(`#b${j}`).remove()
+		removeFromSum(n, suly) 
+	}
+
 	crn.appendChild(crn_removebutton);
 
-
 	document.querySelector("#jegyek").appendChild(crn);
+
+	sum.push([n, suly])
+	calcAv();
 }
